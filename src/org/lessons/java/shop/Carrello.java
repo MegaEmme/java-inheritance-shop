59327bf4c6eba7cfde5261f79a2b52f52902ad88
MegaEmme <mikmusco@gmail.com>
2025-07-09 11:56:56 +0200

    feat: create Carrello class fix:last commit --> updated Prodotto class

---
+++ src/org/lessons/java/shop/Carrello.java
@@ -0,0 +1,31 @@
+package org.lessons.java.shop;
+
+import java.util.Scanner;
+
+public class Carrello {
+    public static void main(String[] args) {
+        System.out.println("Benvenuto nello shop");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(
+                "Stai acquistando: Smartphone, TV o Cuffie? (puoi anche digitare 1, 2 o 3 per le rispettive sezioni)");
+
+        String prodottoCarrello = scan.nextLine().toLowerCase().replaceAll("\\s", "");
+
+        if (prodottoCarrello.equalsIgnoreCase("smartphone") || prodottoCarrello.equals("1")) {
+            System.out.println("Benvenuto nella sezione Smartphone");
+        } else if (prodottoCarrello.equalsIgnoreCase("tv") || prodottoCarrello.equals("2")) {
+            System.out.println("Benvenuto nella sezione TV");
+        } else if (prodottoCarrello.equalsIgnoreCase("cuffie") || prodottoCarrello.equals("3")) {
+            System.out.println("Benvenuto nella sezione Cuffie");
+        } else {
+            System.out
+                    .println(
+                            "Prodotto non presente...\n - Inserisci una categoria tra Smartphone (1), TV(2) o Cuffie(3)");
+        }
+
+        scan.close();
+
+    }
+}
